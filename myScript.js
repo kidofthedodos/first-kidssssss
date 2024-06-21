d0e228e25f7396f9f4bc3b84be68d013e971b6c9
kidofthedodos <163680290+kidofthedodos@users.noreply.github.com>
2024-06-21 17:02:28 -0500

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,20 +1,2 @@
 function myFunction() {
     document.getElementById("demo").innerHTML="Biruk is a bot at basketball"
-document.addEventListener("DOMContentLoaded", function() {
-  const button = document.getElementById("changeColorBtn");
-  const body = document.body;
-
-  button.addEventListener("click", function() {
-    const randomColor = generateRandomColor();
-    body.style.backgroundColor = randomColor;
-  });
-
-  function generateRandomColor() {
-    const letters = "0123456789ABCDEF";
-    let color = "#";
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  }
-});
